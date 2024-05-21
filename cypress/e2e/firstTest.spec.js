295da03c83622ad29530c74c55df1b27355cc6aa
Danila Albutov <danil.miheev2222@gmail.com>
2024-05-20 17:27:02 -0700

    date picker in current month

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -121,7 +121,7 @@ describe('First test suite',()=>{
             cy.wrap(radioButtons).eq(2).should('be.disabled')
         })
     })
-    it.only('check boxes',()=>{
+    it('check boxes',()=>{
         cy.visit('/')
         cy.contains('Modal & Overlays').click()
         cy.contains('Toastr').click() 
@@ -131,20 +131,24 @@ describe('First test suite',()=>{
         
         cy.get('[type="checkbox"]').eq(0).click({force:true})
     })
+    it.only('Date picker',()=>{
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Datepicker').click() 
+
+        // js object to pick always 5 day later
+        let date = new Date()
+        date.setDate(date.getDate() + 5)
+        let futureDate = date.getDate()
+        let dateToAssert = `May ${futureDate}, 2024`
 
 
-// describe('Second test suite',()=>{
+        cy.contains('nb-card','Common Datepicker').find('input').then(input =>{
+            cy.wrap(input).click()
+            cy.get('.day-cell').not('.bounding-month').contains(futureDate).click()
+            cy.wrap(input).invoke('prop','value').should('contain',dateToAssert)
+            //second option for assertion this date and its much easier
+            cy.wrap(input).should('have.value',dateToAssert)
 
-//     it('first test',()=>{
-//         // code of the test
-//     })
-
-//     it('second test',()=>{
-//         // code of the test
-//     })
-
-//     it('third test',()=>{
-//         // code of the test
-//     })
-    
-// })
+        })
+    })
